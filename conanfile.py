79aa9484e13265b8c7e252b088140cedc97a8fbd
Malibushko <malibushko@outlook.com>
2020-08-01 14:40:31 +0300

    Add verbosity level variable to CMakeList and modified conanfile to reflect new option

--- conanfile.py
+++ conanfile.py
@@ -11,29 +11,37 @@ class TGlibConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
    generators = "cmake"
    requires = "rapidjson/1.1.0","cpp-httplib/0.5.7","gtest/1.10.0","magic_get/1.0"
-   options = {"build_examples": [True, False], "build_tests": [True,False]}
-   default_options = {"build_examples": False, "build_tests":True, "cpp-httplib:with_openssl": True}
+   options = {"build_examples": [True, False], "build_tests": [True,False],"verbosity_level":[0,1,2]}
+   default_options = {"build_examples": False, "build_tests":True, "cpp-httplib:with_openssl": True,"verbosity_level":1}
 
    def source(self):
-       self.run("git clone https://github.com/Malibushko/yatgbotlib.git")
-       try:
-           if os.environ['TRAVIS_BUILD']:
-               print("Building on Travis")
-               self.run("pip install html2markdown lxml beautifulsoup4")
-       except KeyError: 
-           print("Building on local machine")
-           self.run("pip install --user html2markdown lxml beautifulsoup4")
+      self.run("git clone https://github.com/Malibushko/yatgbotlib.git")
+      try:
+          if os.environ['TRAVIS_BUILD']:
+              print("Building on Travis")
+              self.run("pip install html2markdown lxml beautifulsoup4")
+      except KeyError: 
+          print("Building on local machine")
+          self.run("pip install --user html2markdown lxml beautifulsoup4") 
+          
+           
+   def configure_cmake(self):
+       cmake = CMake(self)
+
+       cmake.definitions["TGLIB_BUILD_EXAMPLES"] = "ON" if self.options.build_examples else "OFF"
+       cmake.definitions["TGLIB_BUILD_TESTS"] = "ON" if self.options.build_tests else "OFF"
+       cmake.definitions["VERBOSITY_LEVEL"] = self.options.verbosity_level
+       print("Building tests: {}\nBuilding examples: {}\nLogger verbosity level: {}"
+       	.format(cmake.definitions["TGLIB_BUILD_TESTS"],cmake.definitions["TGLIB_BUILD_EXAMPLES"],cmake.definitions["VERBOSITY_LEVEL"]))
+       cmake.configure()
+       return cmake
 
    def build(self):
-      cmake = CMake(self)
+      cmake = self.configure_cmake()
       cmake.verbose = True
-      
-      examples = "-DTGLIB_BUILD_EXAMPLES=ON" if self.options.build_examples else "-DTGLIB_BUILD_EXAMPLES=OFF"
-      tests = "-DTGLIB_BUILD_TESTS=ON" if self.options.build_tests else "-DTGLIB_BUILD_TESTS=OFF";
-      self.run("cmake yatgbotlib/ %s %s" % (examples,tests))
-      cmake.build();
-      if self.options.build_examples:
-         self.run("ctest");
+      cmake.build()
+      if self.options.build_tests:
+         cmake.test()
 
    def package(self):
       self.copy("*", dst="bin", src="./bin") # From bin to bin
